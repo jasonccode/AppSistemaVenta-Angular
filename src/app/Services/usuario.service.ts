5f2e09206999686cc512de2e08680bcc1b29aa92
Jason C <grousemusic@gmail.com>
2023-12-12 19:14:34 -0500

    Implementando UsuarioService

--- src/app/Services/usuario.service.ts
+++ src/app/Services/usuario.service.ts
@@ -1,11 +1,39 @@
+import { Login } from './../Interfaces/login';
+import { ResponseApi } from './../Interfaces/response-api';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../Interfaces/usuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
+// Proporciona funciones para iniciar sesión, obtener lista, guardar, editar y eliminar usuarios.
+// Interfaz para interactuar con las operaciones relacionadas con usuarios en el backend a través de solicitudes HTTP
 export class UsuarioService {
+  private urlApi: string = environment.endpoint + 'Usuario/';
 
-private 
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  iniciarSesion(request: Login): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.urlApi}IniciarSesion`, request);
+  }
+
+  lista(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`);
+  }
+
+  guardar(request: Usuario): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.urlApi}Guardar`, request);
+  }
+
+  editar(request: Usuario): Observable<ResponseApi> {
+    return this.http.put<ResponseApi>(`${this.urlApi}Editar`, request);
+  }
+
+  eliminar(id: number): Observable<ResponseApi> {
+    return this.http.delete<ResponseApi>(`${this.urlApi}Eliminar/${id}`);
+  }
 }
