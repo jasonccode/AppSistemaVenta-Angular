49eee05059f44fd4a784dff417da26da29727c10
Jason C <grousemusic@gmail.com>
2023-12-18 13:06:59 -0500

    Configurando dashboar- graficos

--- src/app/Components/layout/Pages/dash-board/dash-board.component.ts
+++ src/app/Components/layout/Pages/dash-board/dash-board.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { DashBoardService } from 'src/app/Services/dash-board.service';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
-  styleUrls: ['./dash-board.component.css']
+  styleUrls: ['./dash-board.component.css'],
 })
-export class DashBoardComponent {
+export class DashBoardComponent implements OnInit {
+  totalIngresos: string = '0';
+  totalVentas: string = '0';
+  totalProductos: string = '0';
 
+  constructor(private _dashboarServicio: DashBoardService) {}
+
+  ngOnInit(): void {
+    this._dashboarServicio.resumen().subscribe({
+      next: (data) => {
+        if (data.status) {
+          this.totalIngresos = data.value.totalIngresos;
+          this.totalVentas = data.value.totalVentas;
+          this.totalProductos = data.value.totalProductos;
+
+          const arrayData: any[] = data.value.ventasUltimaSemana;
+          const labelTemp = arrayData.map((value) => value.fecha);
+          const dataTemp = arrayData.map((value) => value.total);
+          this.mostrarGrafico(labelTemp, dataTemp);
+        }
+      },
+      error: (e) => {},
+    });
+  }
+
+  mostrarGrafico(labelGrafico: any[], dataGrafico: any[]) {
+    const chartBarras = new Chart('chartBarras', {
+      type: 'bar',
+      data: {
+        labels: labelGrafico,
+        datasets: [
+          {
+            label: '# de ventas',
+            data: dataGrafico,
+            backgroundColor: ['rgba(54,162,235,0.2)'],
+            borderColor: ['rgba(54,162,235,1)'],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: { y: { beginAtZero: true } },
+      },
+    });
+  }
 }
